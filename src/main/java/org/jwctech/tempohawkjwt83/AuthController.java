4aa0add6e29bb63864f12ac6830457223f74020f
James <jamesrwyatt35@gmail.com>
2024-04-10 13:15:49 -0400

    Created Auth Endpoints

--- src/main/java/org/jwctech/tempohawkjwt83/AuthController.java
+++ src/main/java/org/jwctech/tempohawkjwt83/AuthController.java
@@ -11,12 +11,47 @@ import java.util.Map;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final JwtService jwtService;
 
+    @PostMapping (path = "/signup", consumes = APPLICATION_JSON_VALUE)
+    public String signUp() {
+        return "Signed Up!";
+    }
+
+    @PostMapping(path = "/signIn", consumes = APPLICATION_JSON_VALUE)
+    public String signIn() {
+        return "Signed In!";
+    }
+
+    @PostMapping(path = "/signout", consumes = APPLICATION_JSON_VALUE)
+    public String signOut() {
+        return "Signed Out!";
+    }
+
+    @PostMapping(path = "/remove", consumes = APPLICATION_JSON_VALUE)
+    public String remove() {
+        return "User removed!";
+    }
+
+    @PostMapping(path = "/token", consumes = APPLICATION_JSON_VALUE)
+    public String validateToken() {
+        return "Valid token!";
+    }
+
+    @PostMapping(path = "/refresh", consumes = APPLICATION_JSON_VALUE)
+    public String refreshToken() {
+        return "new refresh token!";
+    }
+
+    /**
+     * Method to Create a JWT with Claims
+     * @param claims - Key value pair of info for JWT
+     * @return JWT with Claims
+     */
     @PostMapping(path = "/token", consumes = APPLICATION_JSON_VALUE)
     public String getToken(@RequestBody Map<String, Object> claims) {
         return jwtService.generateJWT(claims);
--- src/main/java/org/jwctech/tempohawkjwt83/SecurityConfig.java
+++ src/main/java/org/jwctech/tempohawkjwt83/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                         //Allow all request for home page with permit ALl
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/health-check").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/secured/admin").hasAuthority("SCOPE_ADMIN")
                         //AnyRequest is a catch-all for any request that doesn't match the above
                         .anyRequest().authenticated()
