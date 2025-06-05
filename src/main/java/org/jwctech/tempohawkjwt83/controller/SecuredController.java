d609839cb62742348943ff9f5385f222b7ba529b
James Wyatt <jamesrwyatt35@gmail.com>
2025-06-05 18:19:01 -0400

    update controller

---
+++ src/main/java/org/jwctech/tempohawkjwt83/controller/SecuredController.java
@@ -0,0 +1,35 @@
+package org.jwctech.tempohawkjwt83.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/secured")
+public class SecuredController {
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_GUEST')")
+    public String guest(Authentication authentication) {
+        return "Hello, %s. You have next permissions: %s"
+                .formatted(getUsername(authentication), authentication.getAuthorities());
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('SCOPE_GUEST')")
+    public String admin(Authentication authentication) {
+        return "Hello, %s. You have next permissions: %s"
+                .formatted(getUsername(authentication), authentication.getAuthorities());
+    }
+
+    private String getUsername(Authentication authentication) {
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        return jwt.getClaimAsString("username");
+    }
+
+}
